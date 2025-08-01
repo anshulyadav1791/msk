075a21ab6ddc955e2e1e08a20ad4723c5c227c7c
anshu <anshulyadav1791@gmail.com>
2025-08-01 12:15:12 +0530

    Update msk.js

--- msk.js
+++ msk.js
@@ -43,23 +43,6 @@ const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector ('.btnLogin-popup');
 const iconclose = document.querySelector('.icon-close');
 
-// registerLink.addEventListener('click',() => {
-//   wrapper.classList.add('active');
-//   });
-
-//   loginLink.addEventListener('click',() => {
-//     wrapper.classList.remove('active');
-//   });
-
-  
-//   btnPopup.addEventListener('click',() => {
-//     wrapper.classList.add('active-popup');
-//   });
-
-  
-//   iconclose.addEventListener('click',() => {
-//     wrapper.classList.remove('active-popup');
-//   });
 
 function openPopup() {
       popup.classList.add("active");
